5df97a2c72b41b2fa2b61865bb7e3d2a26d54e8d
D1114181041 <D1114181041@gm.lhu.edu.tw>
2024-12-20 15:38:31 +0800

    只做了一些些程式碼範例上去 內容未修改

--- resources/views/Alts/create.blade.php
+++ resources/views/Alts/create.blade.php
@@ -1,4 +1,8 @@
 @extends('app')
 @section('Alts.index')
 <h1>新增表單</h1>
-@endsection
+{!! Form::open(['url' => 'players/store'])!!}
+    <div class="from-group">
+        {!! Form::label('project_name','計畫/案件名稱:')!!}
+        {!! Form::text('project_name',null, ['class' => 'form-control'])!!}
+    </div>
